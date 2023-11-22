70575ad3295ef8e24e11759d3496d3c72898dad0
Ishwar Gupta <76861400+ish-g@users.noreply.github.com>
2023-11-22 21:36:19 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def show_teams_name():
-    data1 = requests.get("https://bagmax3443.pythonanywhere.com/").json()
+    data1 = requests.get("https://iplapiishg.pythonanywhere.com/").json()
     l1 = list(data1['_All Teams Name']['team_names'].values())
     return l1
 
@@ -85,30 +85,30 @@ def main():
         j = st.selectbox("Select a 2nd IPL team here", l2)
         enter1 = st.button("Enter", key="4554_1")
         if enter1:
-            response = requests.get(f"https://bagmax3443.pythonanywhere.com/api/teamvteam?team1={i}&team2={j}").json()
+            response = requests.get(f"https://iplapiishg.pythonanywhere.com/api/teamvteam?team1={i}&team2={j}").json()
             return teamvteam(response)
 
     with tab3:
         i3 = st.selectbox("Select any Team here", show_teams_name())
         enter = st.button("Enter", key="4554_3")
         if enter:
-            response3 = requests.get(f"https://bagmax3443.pythonanywhere.com/api/team-record?team={i3}").json()
+            response3 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/team-record?team={i3}").json()
             return team_record_section(response3)
 
     with tab4:
         i4 = st.selectbox("Select a IPL team here", show_teams_name())
         enter = st.button("Enter", key="4554_4")
         if enter:
-            response4 = requests.get(f"https://bagmax3443.pythonanywhere.com/api/team-record?team={i4}").json()
+            response4 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/team-record?team={i4}").json()
             return win_pct(response4)
 
     with tab5:
-        response5 = requests.get(f"https://bagmax3443.pythonanywhere.com/api/batsman-names").json()
+        response5 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/batsman-names").json()
         l5 = response5.get("Batsman Names", {})
         i5 = st.selectbox("Select the Player name here", l5)
         enter = st.button("Enter", key='4554_5')
         if enter:
-            response6 = requests.get(f"https://bagmax3443.pythonanywhere.com/api/batsman-record?batsman={i5}").json()
+            response6 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/batsman-record?batsman={i5}").json()
             batsman_record(response6)
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
