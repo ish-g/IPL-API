b7e8cfe03baf09baefb06bb2053f1e4039606f48
Ishwar Gupta <76861400+ish-g@users.noreply.github.com>
2023-11-26 20:04:00 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def show_teams_name():
-    data1 = requests.get("https://iplapiishg.pythonanywhere.com/").json()
+    data1 = requests.get("https://ishwargupta4554.pythonanywhere.com/").json()
     l1 = list(data1['_All Teams Name']['team_names'].values())
     return l1
 
@@ -61,18 +61,19 @@ def batsman_record(response6):
     with col1:
         st.write("Four on over balls")
         fig1, axe1 = plt.subplots()
-        axe1.pie(four_counts, labels=np.arange(1, len(four_counts)+1), autopct="%0.1f%%")
+        axe1.pie(four_counts, labels=np.arange(1, len(four_counts) + 1), autopct="%0.1f%%")
         st.pyplot(fig1)
     with col2:
         st.write("Six on over balls")
         fig2, axe2 = plt.subplots()
-        axe2.pie(six_counts, labels=np.arange(1, len(six_counts)+1), autopct="%0.1f%%")
+        axe2.pie(six_counts, labels=np.arange(1, len(six_counts) + 1), autopct="%0.1f%%")
         st.pyplot(fig2)
 
 
 def main():
     st.title("Welcome to IPL Stats OceanApp")
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Teams Name", "Team vs Team Record", "Team Record", "Win %", "Player's(4/6)"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        ["Teams Name", "Team vs Team Record", "Team Record", "Win %", "Player's(4/6)"])
 
     with tab1:
         dict1 = {'IPL team names': show_teams_name()}
@@ -85,31 +86,34 @@ def main():
         j = st.selectbox("Select a 2nd IPL team here", l2)
         enter1 = st.button("Enter", key="4554_1")
         if enter1:
-            response = requests.get(f"https://iplapiishg.pythonanywhere.com/api/teamvteam?team1={i}&team2={j}").json()
+            response = requests.get(
+                f"https://ishwargupta4554.pythonanywhere.com/api/teamvteam?team1={i}&team2={j}").json()
             return teamvteam(response)
 
     with tab3:
         i3 = st.selectbox("Select any Team here", show_teams_name())
         enter = st.button("Enter", key="4554_3")
         if enter:
-            response3 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/team-record?team={i3}").json()
+            response3 = requests.get(f"https://ishwargupta4554.pythonanywhere.com/api/team-record?team={i3}").json()
             return team_record_section(response3)
 
     with tab4:
         i4 = st.selectbox("Select a IPL team here", show_teams_name())
         enter = st.button("Enter", key="4554_4")
         if enter:
-            response4 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/team-record?team={i4}").json()
+            response4 = requests.get(f"https://ishwargupta4554.pythonanywhere.com/api/team-record?team={i4}").json()
             return win_pct(response4)
 
     with tab5:
-        response5 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/batsman-names").json()
+        response5 = requests.get(f"https://ishwargupta4554.pythonanywhere.com/api/batsman-names").json()
         l5 = response5.get("Batsman Names", {})
         i5 = st.selectbox("Select the Player name here", l5)
         enter = st.button("Enter", key='4554_5')
         if enter:
-            response6 = requests.get(f"https://iplapiishg.pythonanywhere.com/api/batsman-record?batsman={i5}").json()
+            response6 = requests.get(
+                f"https://ishwargupta4554.pythonanywhere.com/api/batsman-record?batsman={i5}").json()
             batsman_record(response6)
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
